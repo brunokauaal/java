2620b1ddea7fa41b9209eb3b01879a8210fa8eb8
Bruno Kauã <brunokauaalvesdecarvalho@gmail.com>
2023-03-14 11:52:20 -0300

    Projeto Hello World

---
+++ helloword/src/helloword/HelloWord.java
@@ -0,0 +1,13 @@
+package helloword;
+
+public class HelloWord {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int teste = 10;
+		System.out.println("Hello Word!");
+		System.out.println("Olá Turma 62");
+		System.out.println("Variavel teste: "+ teste);
+	}
+
+}
